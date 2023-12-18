d3e14ee18b4d9ca1ae9f612148eb2450c317e631
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2023-12-18 20:57:30 +0300

    project created

---
+++ Timer/Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Roman Litvinovich on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
